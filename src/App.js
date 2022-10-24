3fda26f674355a35e1acf2be0f9d7aa04b04a503
eduardowanderleyde <wanderley.eduardo@gmail.com>
2022-10-24 19:57:57 -0300

    “input“

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">ola</div>;
-}
-
-export default App;
---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import gitLogo from "../assets/github.png";
+import Input from "../components/Input";
+
+import { Container } from "./styles";
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="github logo" />
+      <Input />
+    </Container>
+  );
+}
+
+export default App;
